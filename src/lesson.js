032d27836e7f6081e4e2228501b9635bb9ef2c5d
Brandon Wong <bwong@societekolab.com>
2017-04-07 01:29:37 -0400

    initial proof-of-concept (with file watchers) complete

--- src/lesson.js
+++ src/lesson.js
@@ -1,14 +1,29 @@
 
+const slides = {
+    welcome: '(1/3) Welcome to my lesson!\nBegin by creating a file called one.txt.',
+    filecreated: '(2/3) Good! Now type in the file "hello world!" and save it.',
+    content: '(3/3) Well done! This lesson is now complete.',
+};
+
 export default function lesson(API) {
     API.watcher.on('all', function happen(ev, path) {
-        console.log('file event', ev, path);
+        //console.log('file event', ev, path);
+        if( path === 'one.txt' ) {
+            if( ev === 'unlink' ) {
+                API.presentSlide(slides.welcome);
+            }
+            else {
+                const contains = API.fileContents(path).indexOf('hello world!') !== -1;
+                if( contains ) {
+                    API.presentSlide(slides.content);
+                }
+                else {
+                    API.presentSlide(slides.filecreated);
+                }
+            }
+        }
     });
-    API.watcher.on('ready', function () {
-        console.log('READY!', arguments);
-    });
-    API.watcher.on('raw', function () {
-        console.log('raw', arguments);
-    });
+    API.presentSlide('Hi! This is the "Hello World!" demo of Code Companion!');
 };
 
 
--- src/main.js
+++ src/main.js
@@ -1,21 +1,45 @@
 
 import chokidar from 'chokidar';
+import { readFileSync } from 'fs';
 
 import lesson from './lesson.js';
 
+const selectedFolder = 'files'
+
 function createAgent() {
-    const fileWatcher = chokidar.watch('files');
+    const fileWatcher = chokidar.watch(selectedFolder);
+
+    function pathFilter(path) {
+        if( typeof path !== 'string' ) {
+            return path;
+        }
+        return path.replace(/^files\//g, '');
+    }
 
     return {
         watcher: {
             on(evt, cb) {
-                fileWatcher.on(evt, cb);
+                fileWatcher.on(evt, function filterOutPathAndCall(...args) {
+                    const newArgs = args.map(pathFilter);
+                    cb(...newArgs);
+                });
             },
+        },
+        fileContents(path) {
+            return readFileSync(`${selectedFolder}/${path}`, 'UTF8');
+        },
+        presentSlide(content) {
+            console.log();
+            console.log('>>>>>>>>>> SLIDE <<<<<<<<<<');
+            console.log(content);
+            console.log('>>>>>>>>>>>>>o<<<<<<<<<<<<<');
+            console.log();
         },
     };
 }
 
 
+console.log('loading lesson...');
 lesson( createAgent() );
 
 
